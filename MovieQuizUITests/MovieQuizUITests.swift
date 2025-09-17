2f94d13bfc49d362f0f9ea9b9b473748cd96ff65
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2025-09-17 19:33:03 +0300

    Тесты

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,54 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Denis Bokov on 16.09.2025.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+class MovieQuizUITests: XCTestCase {
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        // это специальная настройка для тестов: если один тест не прошёл,
+        // то следующие тесты запускаться не будут; и правда, зачем ждать?
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+                
+        app.terminate()
+        app = nil
+    }
+
+    @MainActor
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+    
+    func testYesButton() {
+        sleep(3)
+        let firstPoster = app.images["Poster"] // находим первоначальный постер
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        XCTAssertTrue(firstPoster.exists)
+        
+        app.buttons["Yes"].tap() // находим кнопку `Да` и нажимаем её
+        
+        sleep(3)
+        let secondPoster = app.images["Poster"] // ещё раз находим постер
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        XCTAssertTrue(secondPoster.exists)
+        
+        XCTAssertFalse(firstPosterData == secondPosterData) // проверяем, что постеры разные
+//        XCTAssertNotEqual(firstPosterData, secondPosterData) // второй вариант проверки
+    }
+}
